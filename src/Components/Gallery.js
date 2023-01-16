ce474d589e52048ed74766cf978b0816a903b4ec
FCHDev <acetige@gmail.com>
2023-01-16 21:52:28 +0100

    Downscale images

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -28,7 +28,9 @@ const Gallery = () => {
                     return 0.5 - Math.random();
                 }).map((pic, index) =>
                     <div className="gallery-item" key={index} onClick={() => getImg(pic.img, pic?.title)}>
-                        <img className="block object-cover object-center w-full h-full rounded-lg hover:opacity-50"
+                        <img className="
+                        block object-cover object-center w-full h-full rounded-lg hover:opacity-50
+                        transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-75"
                              src={pic.img}
                              alt={pic?.title}
                              title={pic?.title}/>
@@ -40,13 +42,15 @@ const Gallery = () => {
     return (
         <>
             <div className={`${!modal ? "modal" : "modal open relative"}`}>
-                <img src={holdSrc} alt={holdTitle}/>
+                <img className="rounded-xl" src={holdSrc} alt={holdTitle}/>
                 <img className="fixed md:top-10 md:left-10 bottom-7 h-8 cursor-pointer opacity-50"
                      onClick={() => closeImg()}
                      src={closeIcon}
                      alt="fermer close"/>
                 <span
-                    className="fixed bg-black text-white md:text-3xl md:right-1 md:top-4 text-base top-3 px-2 rounded-xl">
+                    className="fixed bg-black text-white
+                    md:text-3xl md:right-1 md:top-4
+                    text-base top-3 px-2 rounded-xl">
                     {holdTitle}
                 </span>
             </div>
