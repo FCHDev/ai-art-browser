0f79d107fd2bb14e2cd17e9fc466113b5b1a9aa3
FCHDev <acetige@gmail.com>
2023-02-01 17:55:46 +0100

    Admin Page work / Listing

--- src/Pages/AdminPage.js
+++ src/Pages/AdminPage.js
@@ -1,44 +1,41 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import {AiFillCaretLeft} from "react-icons/ai";
 
-import {set} from "firebase/database";
+import {onValue, ref, set} from "firebase/database";
 import {refDb} from "../service/firebase-config";
 import {db} from "../service/firebase-config";
 
-const AdminPage = ({artworks, totalArtwork}) => {
+const AdminPage = ({artworks, setArtworks, totalArtwork}) => {
     /// UPLOAD IMAGES
     const [imageUpload, setImageUpload] = useState(null);
     const [picPreview, setPicPreview] = useState();
 
-    // const uploadImage = () => {
-    //     if (imageUpload == null) return;
-    //     const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
-    //     uploadBytes(imageRef, imageUpload)
-    //         .then((snapshot) => {
-    //             getDownloadURL(snapshot.ref).then((url) => {
-    //                 setImgURL(url);
-    //                 setPicPreview(
-    //                     <div className="artwork-preview">
-    //                         <img className="rounded-xl" src={url} alt="artwork"/>
-    //                     </div>
-    //                 );
-    //             });
-    //             // alert("Image uploaded");
-    //         })
-    //         .catch((error) => {
-    //             console.error(error);
-    //         });
-    // };
-
-
     // STATES
     const creationDate = new Date().getTime()
-    const id = imgCount()
+    const [id, setId] = useState("")
     const [imgURL, setImgURL] = useState("");
     const [title, setTitle] = useState("")
+
+    // CALL BASE POUR LISTING
+    useEffect(() => {
+        onValue(ref(db), (snapshot) => {
+            const data = snapshot.val();
+            if (data !== null) {
+                // eslint-disable-next-line
+                Object.values([data]).map((item) => {
+                    setArtworks(Object.values(item));
+                    setId(Object.values(item).length);
+                    console.log("🔥🔥🔥🔥 Firebase is called 🔥🔥🔥🔥");
+                });
+            } else {
+                throw new Error("Il y a un souci");
+            }
+        });
+        // eslint-disable-next-line
+    }, []);
 
 
     // HANDLES
@@ -49,6 +46,15 @@ const AdminPage = ({artworks, totalArtwork}) => {
         setTitle(event.target.value);
     };
 
+    const handleSortTitle = (event) => {
+        event.preventDefault()
+        artworks.sort((a, b) => (a.title > b.title ? 1 : -1))
+    }
+    const handleSortId = (event) => {
+        event.preventDefault()
+        setArtworks(artworks.sort((a, b) => (a.id > b.id ? 1 : -1)))
+    }
+
     const reset = () => {
         setPicPreview(null);
         setImageUpload(null);
@@ -56,10 +62,6 @@ const AdminPage = ({artworks, totalArtwork}) => {
         setImgURL("")
     }
 
-    function imgCount() {
-        const storedImg = JSON.parse(localStorage.getItem('localArtworks'));
-        return storedImg.length
-    }
 
     // FONCTION POUR ÉCRIRE UN NOUVEL ARTWORK SUR FIREBASE
     const writeUserData = () => {
@@ -82,6 +84,11 @@ const AdminPage = ({artworks, totalArtwork}) => {
             title: path
         });
         console.log("Update done !")
+    }
+
+    // FONCTION POUR SUPPRIMER UN ARTWORK SUR FIREBASE
+    const handleRemove = (idToRemove) => {
+        console.log(idToRemove);
     }
 
     // POUR ENREGISTRER LES DONNEES AVANT SOUMISSION
@@ -206,6 +213,69 @@ const AdminPage = ({artworks, totalArtwork}) => {
 
             {picPreview}
 
+            {/*APERCU DE LA LISTE DES ARTWORKS*/}
+            <div className="hidden md:block w-1/2">
+                <div className="sm:flex sm:items-center">
+                    <div className="sm:flex-auto">
+                        <h1 className="text-xl md:text-2xl font-semibold text-[#174A5B]">Artworks DB</h1>
+                        <p className="mt-2 text-sm text-gray-700">
+                            Ici, tu retrouves la liste de tous les artworks.
+                        </p>
+                    </div>
+                </div>
+                <div className="mt-8 flex flex-col">
+                    <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+                                <table className="min-w-full divide-y divide-gray-300">
+                                    <thead className="bg-gray-50">
+                                    <tr>
+                                        <th scope="col"
+                                            className="py-3.5 pl-4 pr-3 text-left text-base font-semibold text-gray-900 sm:pl-6 cursor-pointer"
+                                            onClick={handleSortId}>
+                                            ID
+                                        </th>
+                                        <th scope="col"
+                                            className="px-3 py-3.5 text-left text-base font-semibold text-gray-900 cursor-pointer"
+                                            onClick={handleSortTitle}>
+                                            Titre
+                                        </th>
+                                        <th scope="col"
+                                            className="px-3 py-3.5 text-left text-base font-semibold text-gray-900 cursor-pointer"
+                                            onClick={() => console.log("Prout ")}>
+                                            URL Image
+                                        </th>
+
+                                    </tr>
+                                    </thead>
+                                    <tbody className="bg-white">
+                                    {artworks
+                                        .map((artwork, index) => (
+                                            <tr key={index} className={index % 2 === 0 ? undefined : 'bg-[#CEEAF3]'}>
+                                                <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                                    {artwork.id.length > 5 ? artwork.id.substring(0, 5) + "..." : artwork.id}
+                                                </td>
+                                                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 overflow-hidden">
+                                                    {artwork.title}
+                                                </td>
+                                                <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 overflow-hidden">
+                                                    {artwork.imgURL.length > 40 ? artwork.imgURL.substring(0, 40) + "..." : artwork.imgURL}
+                                                </td>
+                                                <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                                    <a href="#" className="text-[#FF585D] hover:text-[#FFC2C4]"
+                                                       onClick={() => handleRemove(artwork.id)}>
+                                                        Supprimer
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
