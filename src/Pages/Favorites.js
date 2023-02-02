8d30674074dc2e7188f58d5c236eed8ec5eeb259
FCHDev <acetige@gmail.com>
2023-02-02 17:35:10 +0100

    New feature : Carousel on Fav Page

--- src/Pages/Favorites.js
+++ src/Pages/Favorites.js
@@ -2,6 +2,11 @@ import React from 'react';
 import {remove} from "firebase/database";
 import {db, refDb} from "../service/firebase-config";
 import {useUserContext} from "../Context/Context";
+import closeIcon from '../Assets/SVG/close.svg'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 const Favorites = ({personalFav, setPersonalFav}) => {
     // DÉCLARATION DU CONTEXTE USER
@@ -20,30 +25,56 @@ const Favorites = ({personalFav, setPersonalFav}) => {
         }
     };
 
-    // SUPPRIMER LE FAVORI SÉLECTIONNÉ
+    // SUPPRIMER UNIQUEMENT LE FAVORI SÉLECTIONNÉ
     const removeThisFav = (titleToRemove) => {
         remove(refDb(db, `/fav/${user.id}/${titleToRemove}`), {})
     };
 
+    // CAROUSEL
+    function Carousel() {
+        const settings = {
+            className: "center",
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            arrows: false,
+            swipeToSlide: true,
+        };
+
+        return (
+            <Slider {...settings}>
+                {personalFav.map((item, index) =>
+                    <div key={index} className="mb-10">
+                        <img src={item.src} alt={item.title} className="mx-auto rounded-xl object-fit max-h-[700px]"/>
+                    </div>)}
+            </Slider>
+        );
+    }
+
 
     return (
-        <div>
+        <div className="flex flex-col">
             <h1 className="text-2xl text-center my-10">Espace Favoris de <strong className="text-[crimson]">
                 {user.firstName}
             </strong>
             </h1>
-            <div className="flex flex-wrap justify-evenly border w-3/4 mx-auto p-3 rounded-xl">
+
+                <Carousel/>
+
+            <div className="flex flex-wrap justify-evenly border border-white border-opacity-20 w-3/4 mx-auto p-3 rounded-xl">
                 {personalFav.length === 0
                     ? "Vous n'avez pas encore ajouté de favoris 🥹"
                     : personalFav.map((item, index) =>
                         <div key={index} className="flex flex-col justify-center items-center relative">
                             <img className="h-[200px] w-auto rounded-xl" src={item.src} alt={item.title}/>
-                            <span
-                                className="absolute top-1 right-1 bg-[crimson] text-white rounded-full px-2 flex justify-center items-center cursor-pointer"
-                                onClick={() => removeThisFav(item.title)}
-                            >
-                                x
-                            </span>
+                            <img src={closeIcon}
+                                 alt="close icon"
+                                 className="h-6 absolute top-1 right-1 bg-[crimson] text-white rounded-full p-1 flex justify-center items-center cursor-pointer"
+                                 onClick={() => removeThisFav(item.title)}
+                            />
                             <span className="text-xs text-center mx-auto">
                                    {item.title}
                                </span>
@@ -57,6 +88,9 @@ const Favorites = ({personalFav, setPersonalFav}) => {
                     Vider Favoris
                 </button>
             </div>
+
+
+
         </div>
     );
 };
